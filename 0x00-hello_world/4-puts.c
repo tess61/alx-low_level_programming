29f8e1e1a5ad59db955e6ba5db3e77b6be5ffe98
Tesfa Melkamu <hopemelkamu28@gmail.com>
2022-09-08 13:18:17 -0700

    question 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 int main ( void )
 {
 	char str1[60];
-	strcpy(str1, "programming is like building a multilingual puzzle");
+	strcpy(str1, "programming is like building a multilingual puzzle\n");
 	puts(str1);
 	return 0;
 }
