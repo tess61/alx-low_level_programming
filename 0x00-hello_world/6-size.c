f4669c058ed00e0125bf8ba1eadfd39a70138995
Tesfa Melkamu <hopemelkamu28@gmail.com>
2022-09-08 14:57:22 -0700

    question 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - Entry poin
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+siezof(a);
+siezof(b);
+siezof(c);
+siezof(d);
+siezof(e);
+return (0);
+}
