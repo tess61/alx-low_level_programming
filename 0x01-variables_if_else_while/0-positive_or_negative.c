af9c5701ca4ed0a794b0dd65e3cc69ee6bbc6b28
Tesfa Melkamu <hopemelkamu28@gmail.com>
2022-09-09 12:01:26 -0700

    q0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,13 +12,13 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n < 0)
 {
-printf("%d is negative", &n);
+printf("%d is negative", n);
 }
 else if (n > 0)
 {
-printf("%d is positive", &n);
+printf("%d is positive", n);
 }
 else
-printf("%d is zero", &n);
+printf("%d is zero", n);
 return (0);
 }
