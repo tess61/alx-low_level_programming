a3128d387ab5c3fe9f105466eb7e431759cf08a0
Tesfa Melkamu <hopemelkamu28@gmail.com>
2022-09-09 16:17:17 -0700

    q9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print decimal
+ * Return: 0 if success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+		if (i == 57)
+			continue;
+		putchar(' ');
+		putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
