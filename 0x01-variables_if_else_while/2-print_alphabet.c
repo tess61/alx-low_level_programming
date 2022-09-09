2235f01d1fa689eb596bbad7f6c33aa787fc679d
Tesfa Melkamu <hopemelkamu28@gmail.com>
2022-09-09 13:44:22 -0700

    q2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+*main - print the last digit of the n valu3
+*Return: always 0 (success)
+*/
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+printf("%c", ch);
+ch++;
+}
+return (0);
+}
