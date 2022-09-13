4d9eca6a1f6986444dae8414bdc586dd279aac5c
Tesfa Melkamu <hopemelkamu28@gmail.com>
2022-09-13 16:58:53 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - check a number
+ * @c: user input
+ * return: 1 positive 
+ * or -1 negative
+ * otherwiese 0 
+ */
+int print_sign(int n)
+{
+	if (c < 0)
+	{
+		return (-1)
+		_putchar('-');
+	}
+	else if (c > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else
+	{
+		return (0);
+		_putchar(0);
+}
