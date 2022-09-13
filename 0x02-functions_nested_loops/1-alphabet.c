90c19d1818e7f8dd96c84d4e6da4ccb6919af5ec
Tesfa Melkamu <hopemelkamu28@gmail.com>
2022-09-13 14:14:45 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - check the code
+ * print_alphabet - print alphabets
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
