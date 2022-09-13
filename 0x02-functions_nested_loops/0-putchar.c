6384552ace3e1ae9fa567ec2f9cd65152fa5adf2
Tesfa Melkamu <hopemelkamu28@gmail.com>
2022-09-13 13:49:35 -0700

    i dont know

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,18 @@
-#include  <stdio.h>
+#include "main.h"
 /**
- * main - prints the word putchar
- * return: 0 if succssed
- */
-int main(void)
+*_putchar - prints the word  _putchar
+*Return: 0 if successed
+*/
+int _putchar(char c)
 {
-	printf("_putchar");
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
 	return (0);
 }
