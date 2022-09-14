c89835f93356d5bb0d1201098e5c35985454633f
tess61 <111118596+tess61@users.noreply.github.com>
2022-09-15 01:18:35 +0300

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,15 +1,1 @@
-#include "main.h"
-/**
- * main - tests funtion that prints if integer is zero
- * Return: 0
- */
 int largest_number(int a, int b, int c);
-int main(void)
-{
-	int i;
-
-	i = 0;
-	positive_or_negative(i);
-
-	return (0);
-}
