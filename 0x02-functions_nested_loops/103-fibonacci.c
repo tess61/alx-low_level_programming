f7dec9b8872cd1da129c6b8b3f3cf973e9d57452
tess61 <111118596+tess61@users.noreply.github.com>
2022-09-14 04:48:46 +0300

    adv 4

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints the first 50 fibonacci numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	long sum = 2;
+	long first = 1;
+	long second = 2;
+	long fib;
+
+	while (fib <= 4000000)
+	{
+		fib = first + second;
+		if (fib % 2 == 0)
+		{
+			sum += fib;
+		}
+		first = second;
+		second = fib;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Term
