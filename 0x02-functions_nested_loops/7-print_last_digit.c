ddbb95351e608ba9a0cf112ea9cf69886284ad76
Tesfa Melkamu <hopemelkamu28@gmail.com>
2022-09-13 17:47:38 -0700

    task10

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - find last digit
+ * @n: useer input
+ * return: the last digit
+ */
+int print_last_digit(int n)
+{
+	int ld = n % 10;
+
+	return (ld);
+}
