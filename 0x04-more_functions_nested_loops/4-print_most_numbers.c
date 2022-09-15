9454efa9f81fb2554ab15c76ec7fdbed1ed489d3
tess61 <111118596+tess61@users.noreply.github.com>
2022-09-16 02:02:40 +0300

    task 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints 0-9 to the standard output, except 2 and 4
+ *
+ * Return: Doesn't return anything, void
+ */
+void print_most_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+		{
+			_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
