aea477e942dfe150696fbc7b6e3e5f89cec46320
Tesfa Melkamu <hopemelkamu28@gmail.com>
2022-09-15 17:28:02 -0700

    task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
  * print_remaining_days - takes  a da
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,2 +1,3 @@
+#include <stdio.h>
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
