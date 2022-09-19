49adfd9201655cb34f29dedd096d63decab61ddb
Tesfa Melkamu <hopemelkamu28@gmail.com>
2022-09-19 14:23:43 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts2 - only even i think
+ * @str: parameter
+ */
+void puts2(char *str)
+{
+	long int i;
+
+	for (i = 0; i < strlen(str) - 1; i = i + 2)
+	{
+		printf("%c", str[i]);
+	}
+	putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
+void rev_string(char *s);
+void puts2(char *str);
