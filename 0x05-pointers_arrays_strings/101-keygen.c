62bcf3881d3a8a3d2939c96251a0d03c65d22906
tess61 <111118596+tess61@users.noreply.github.com>
2022-09-20 01:35:47 +0300

    task 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,22 +1,40 @@
 #include "main.h"
-#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 /**
- * print_array - print an array.
- * @a: the array.
- * @n: array length
+ * main - generate random password
+ * Return: 
  */
-
-void print_array(int *a, int n)
+int main(void)
 {
-	int i;
-
-	for (i = 0; i < n; i++)
+	int i, j, k, s;
+	char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char p[58];
+	srand(time(NULL));
+	while (s != 2772)
 	{
-		printf("%d", a[i]);
-		if (i < n - 1)
+		i = k = s = 0;
+		while ((2772 - 122) > s)
 		{
-			printf(", ");
+			j = rand() % 62;
+			p[i] = c[j];
+			s += c[j];
+			i++;
+		}
+		while (c[k])
+		{
+			if (c[k] == (2772 - s))
+			{
+				p[i] = c[k];
+				s += c[k];
+				i++;
+				break;
+			}
+			k++;
 		}
 	}
-	printf("\n");
+	p[i] = '\0';
+	printf("%s", p);
+	return (0);
 }
