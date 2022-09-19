6b43cb0032a38b22803a47082c1bdf1d72553e6f
Tesfa Melkamu <hopemelkamu28@gmail.com>
2022-09-19 14:02:50 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include <string.h>
+#include <stdio.h>
 /**
  * print_rev - print in reverse
  * @s: parameter
  */
 void print_rev(char *s)
 {
-	unsigned long int i;
+	long int i;
 
 	for (i = strlen(s); i >= 0; i--)
 	{
