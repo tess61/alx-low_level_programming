8834ca3d3920f5548d7ade13e597590b65007aec
Tesfa Melkamu <hopemelkamu28@gmail.com>
2022-09-19 13:05:56 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - return  the value of characters
+ * @s: user input pointe type
+ * Return: numbers fo char in @s
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	for (int i = 0; s[i] != '\0'; i++)
+		len = i + 1;
+	return (len);
+}
