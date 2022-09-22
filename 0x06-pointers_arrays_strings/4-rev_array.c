40986574ac741343283e56cdd5c745a2c1d8fcba
tess61 <111118596+tess61@users.noreply.github.com>
2022-09-23 00:27:32 +0300

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * reverse_array -> functionn
+ * @a: string
+ * @n: no of char
+ */
+void reverse_array(int *a, int n)
+{
+int tmp, index;
+for (index = n - 1; index >= n / 2; index--)
+{
+tmp = a[n - 1 - index];
+a[n - 1 - index] = a[index];
+a[index] = tmp;
+}
+}
