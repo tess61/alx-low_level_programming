08d4dda98a2e984767149af21177c757ea7e62b1
Tesfa Melkamu <hopemelkamu28@gmail.com>
2022-10-10 18:21:11 -0700

    task 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+/**
+ * main - prints the name of the file
+ * Return: 0 if success
+ */
+int main(void)
+{
+	char *buf = __FILE__;
+	int i;
+
+	for (i = 0; buf[i] != '\0'; i++)
+		_putchar(buf[i]);
+	_putchar('\n');
+	return (0);
+}
