5ccff1b06a3088164e65b139ae09e381ed1f5d05
tess61 <111118596+tess61@users.noreply.github.com>
2022-12-15 23:32:37 +0300

    cops

---
+++ 0x18-dynamic_libraries/cops.c
@@ -0,0 +1,54 @@
+/**
+ *add - perform adition
+ *
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ *sub - perform subtraction
+ *
+ * Return: a - b
+ */
+
+int sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ *mul - perform multiplication
+ *
+ * Return: a - b
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ *div - perform div
+ *
+ * Return: a / b
+ */
+
+int div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ *mod - perform mod
+ *
+ * Return: a - b
+ */
+
+int mod(int a, int b)
+{
+return (a % b);
+}
